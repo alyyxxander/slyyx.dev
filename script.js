9b6a2b51215d2459737d46911f40240837129ac1
alyyxxander <alyxs.second.acc@gmail.com>
2022-05-05 17:09:20 -0400

    theme selector functions! site icon (favicon) added

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-//====================================== PUT SOME CODE HERE
+
+
+//this runs when the page is loaded for the first time
+showColorsUsed();
+
+
+
+//====================================== VARIABLES
+var baseColor, wrapperColor, outerColor, innerColor, miscColor;
+
+
 
 
 function setTheme(theme) {
+    //saving the chosen theme in localstorage so that whenever a user comes back 
+    //to the site, it should use the theme previously selected by the user
+    //      figure out why this doesnt work for refreshing the page
+    localStorage.setItem('currentTheme', theme);
+    document.documentElement.className = theme;
+    showColorsUsed();
+}
 
-    switch (theme) {
-        case theme1:
-            
-            break;
+function showColorsUsed() {
 
-        case theme2:
-            
-            break;
-        
-        case theme3:
-            
-            break;
-
-        case theme4:
-            
-            break;
-        
-        default:
-            break;
-    }
-
-
+    //update variables, which change depending onn the current page theme 
+    baseColor = window.getComputedStyle(document.documentElement).getPropertyValue('--base');
+    wrapperColor = window.getComputedStyle(document.documentElement).getPropertyValue('--wrapper');
+    outerColor = window.getComputedStyle(document.documentElement).getPropertyValue('--outer');
+    innerColor = window.getComputedStyle(document.documentElement).getPropertyValue('--inner');
+    miscColor = window.getComputedStyle(document.documentElement).getPropertyValue('--misc');
+    
+    //update the "Colors Used" panel
+    document.getElementById("base-color").innerHTML = "base: " + baseColor;
+    document.getElementById("wrapper-color").innerHTML = "wrapper: " + wrapperColor;
+    document.getElementById("outer-color").innerHTML = "outer: " + outerColor;
+    document.getElementById("inner-color").innerHTML = "inner: " + innerColor;
+    document.getElementById("misc-color").innerHTML = "misc: " + miscColor;
 }
