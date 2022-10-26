f180f010ad01b81bb20ac91efa174ec3be2dbe24
alyyxxander <alyxs.second.acc@gmail.com>
2022-10-26 15:48:49 -0400

    started messing with react

---
+++ demos/svg testing 3/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import DynamicSVG from './DynamicSVG';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <DynamicSVG />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
---
+++ demos/svg testing/dynamic-svg.js
@@ -0,0 +1,44 @@
+// import React from 'react';
+// import MeasureAndRender from "./measure-and-render";
+// import Panel from './panel';
+const {React} = require('react');
+const {MeasureAndRender} = require('./measure-and-render');
+const {Panel} = require('./panel');
+
+export default () => {
+
+  const styles = {
+    position: "relative",
+    display: "grid",
+    gridTemplateColumns: "1fr",
+    gridTemplateRows: "45vh 45vh",
+  };
+
+  const stroke = 1;
+  const offset = 0;
+
+  return (
+    <div style={styles}>
+      <div style={{ position: "relative"}}>
+      <MeasureAndRender stretch={true} debounce={1}>
+        {bounds => {
+          const path = `
+                  M${bounds.width - stroke},${stroke} 
+                  L${stroke},${stroke} 
+                  L${stroke},${bounds.height - stroke} 
+                  L${bounds.width - 77 - offset},${bounds.height - stroke} 
+                  L${bounds.width - 47 - offset},${bounds.height - 30}
+                  L${bounds.width - stroke},${bounds.height - 30}
+                  Z`;
+
+          return <Panel bounds={bounds} path={path} />
+        }}
+      </MeasureAndRender>
+      </div>
+    </div>
+  )
+}
+
+const domContainer = document.querySelector('#svg-container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
